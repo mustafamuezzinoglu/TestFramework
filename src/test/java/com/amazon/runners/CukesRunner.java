47edc4f631c5e8e9e66b1b6c4e3843bb639f0a8a
Mustafa <mustafamuezzinoglu@gmail.com>
2023-02-14 22:52:49 +0300

    plugin html report added

--- src/test/java/com/amazon/runners/CukesRunner.java
+++ src/test/java/com/amazon/runners/CukesRunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
     @RunWith(Cucumber.class)
     @CucumberOptions(
+            plugin = "html:target/cucumber-report.html",
         features = "src/test/resources/Feature",
         glue = "com/amazon/step_definitions"
 )
