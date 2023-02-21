bebe73fd8692875caa1141ad651da0a2d184e027
Mustafa <mustafamuezzinoglu@gmail.com>
2023-02-21 21:40:39 +0300

    library

--- src/test/java/com/amazon/runners/CukesRunner.java
+++ src/test/java/com/amazon/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = "html:target/cucumber-report.html",
-        features = "src/test/resources/Feature",
+        features = "src/test/resources/features",
         glue = "com/amazon/step_definitions",
         dryRun = false,
         tags = "@gmail"
