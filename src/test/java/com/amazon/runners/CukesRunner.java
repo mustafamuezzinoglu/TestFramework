7a74e89315abca7dcb444a7a8c241817a936155d
Mustafa <mustafamuezzinoglu@gmail.com>
2023-03-09 22:51:44 +0300

    Driver class updated

--- src/test/java/com/amazon/runners/CukesRunner.java
+++ src/test/java/com/amazon/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/amazon/step_definitions",
         dryRun = false,
-        tags = "@smoke",
+        tags = "",
         publish = true
 )
 
